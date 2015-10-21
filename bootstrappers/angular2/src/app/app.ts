2deb8ed7051e845c79f13b651084456ad9099457
hanssens <github@hanssens.com>
2015-10-21 09:56:43 +0200

    Added clean, empty Angular2 bootstrapper

---
+++ bootstrappers/angular2/src/app/app.ts
@@ -0,0 +1,13 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'app',
+    template: '<h1>Angulahr 2 App</h1>'
+})
+
+class App { 
+    
+}
+
+bootstrap(App);
+
